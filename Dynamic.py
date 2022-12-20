bcc4261db75c3bff6552e60e4906b94bb7566419
Wisdom Omodiagbe <wisdomosawale@gmail.com>
2022-12-20 00:54:28 -0600

    Just mods

--- Dynamic.py
+++ Dynamic.py
@@ -1,9 +1,65 @@
+from tokenizer import *
+
 class Dynamic:
-    def __init__(self):
+    def __init__(self, name):
+        '''
         self.name = []
         self.age = []
         self.gender = []
         self.color = []
         self.attributes = []
+        '''
+        self.name = name
+        self.category = None
+        self.actions = []
+
+    def action(self, action, other):
+        self.actions.append((action, other))
+        other.actions.append((self, action))
+
+    def print_action(self, index):
+        action = self.actions[index]
+        if type(action[0]) is str:
+            print(self.name, action[0], action[1].name)
+        else:
+            print(self.name, action[1], "by", action[0].name)
+    '''
+    def search(self, verb = None, name=None):
+        #name
+        if verb == None and name!= None:
+            for pair in self.actions:
+                #index 0 is the same
+    '''
+
+    def list_actions(self, index):
+        actions_verbs = []
+        for pair in self.actions:
+            if type(pair[0]) == str:
+                action_verbs.append(pair[0])
+            else:
+                pass
+
+
+
+
+def main():
+    val = input()
+    sentence = Sentence(val)
+    '''
+    for grammar in sentence.grammar_list:
+        s = Dynamic(grammar.subject)
+        o = Dynamic(grammar.obj)
+        s.action(grammar.verb, o)
+        s.print_action(0)
+        o.print_action(0)
+    '''
+
+    grammar = sentence.grammar_list[0]
+    
+    
+if __name__ == '__main__':
+
+    data = []
+    
 
     
--- tokenizer.py
+++ tokenizer.py
@@ -1,5 +1,6 @@
 from itertools import product
 import re
+import scraper
 
 '''
 Problems so far:
@@ -31,57 +32,123 @@ V - AV
 Ultimate form NVN or NV
 '''
 
-N = ['i', 'boy', 'he', 'wisdom', 'glasses']
-V = ['am', 'is', 'was', 'pushed', 'saw']
-D = ['a', 'an', 'the']
-J = ['fast', 'tired', 'smart', 'big']
-P = ['with', 'on', 'in']
+# be, do, have
+verbs = ['is', 'am', 'was', 'were', 'are', 'does', 'did', 'has', 'had']
+
+class POS:
+    def __init__(self, name):
+        self.name = name
+        self.description = []
+        self.link = None
+
+class Noun(POS):
+    def __init__(self, name):
+        self.name = name
+        self.attributes = []
+        self.description = []
+
+class Pronoun(Noun):
+    pass
+
+class Adjective(POS):
+    def __init__(self, name):
+        self.name = name
+        self.description = []
+        self.describes = None
+
+class Verb(POS):
+    def __init__(self, name):
+        self.name = name
+        pass
+
 
 class Grammar:
-    def __init__(self, pos):
+    def __init__(self, pos, tokens = None):
         self.pos = "".join(pos)
-        self.temp = self.pos
+        self.final = self.pos
         self.size = len(self.pos)
         self.spans = []
         self.span_pos = []
-        self.final = None
+        self.tree_pos = None
+        self.tree_token = None
+        self.subject = None
+        self.verb = None
+        self.obj = None
 
         rules = {
             "DN":"N",
             "JN":"N",
-            "NJ":"N",
+            #"NJ":"N",
             "PN":"J",
-            "AV":"V"
+            "AV":"V",
+            "JJ":"J"
             }
 
         while True:
             new = None
             for pattern, val in rules.items():
-                obj = re.search(pattern, self.temp)
+                obj = re.search(pattern, self.final)
                 if obj:
                     span = obj.span()
                     self.spans.append(span)
                     self.span_pos.append(val)
                     #new = re.sub(pattern, val, self.temp)
-                    new = list(self.temp)
+                    new = list(self.final)
                     new[span[0]:span[1]] = val
-                    self.temp = ''.join(new)
+                    self.final = ''.join(new)
             if new == None:
                 break
         val = list(self.pos)
+        val2 = list(tokens)
         for span in self.spans:
             v = tuple(val[span[0]:span[1]])
             val[span[0]:span[1]] = ' '
             val[span[0]] = v
-        self.final = tuple(val)
-                      
+            
+            v2 = tuple(val2[span[0]:span[1]])
+            val2[span[0]:span[1]] = ' '
+            val2[span[0]] = v2
+        self.tree_pos = tuple(val)
+        self.tree_token = tuple(val2)
 
+        # make more general (finding the subject and object)
+
+        #verb
+        '''
+        v_index = self.final.index('V')
+        verb = self.tree_token[v_index]
+
+        if type(verb) is tuple:
+            self.verb = ' '.join(verb)
+        else:
+            self.verb = verb
+
+        #subject
+        for i in range(v_index):
+            if self.final[i] == 'N' or self.final[i] == 'p':
+                subject = self.tree_token[i]
+                if type(subject) is tuple:
+                    self.subject = ' '.join(subject)
+                else:
+                    self.subject = subject
+                break
+
+        #object
+        for i in range(v_index+1, len(self.final)):
+            if self.final[i] == 'N' or self.final[i] == 'p':
+                obj = self.tree_token[i]
+                if type(obj) is tuple:
+                    self.obj = ' '.join(obj)
+                else:
+                    self.obj = obj
+                break
+    '''
 class Sentence:
     def __init__(self, sentence):
-        self.sentence = sentence
+        self.sentence = sentence.lower()
         self.tokens = self.tokenize()
         self.pos_list = self.POS()
-        self.grammar_list = [Grammar(pos) for pos in self.pos_list]
+        self.grammar_list = [Grammar(pos, self.tokens) for pos in self.pos_list]
 
     def tokenize(self):
         first_tokens = self.sentence.split(" ")
@@ -97,7 +164,7 @@ class Sentence:
         pos_list = []
         for token in self.tokenize():
             ans = []
-            # search each part of speech file and add the part of speech to ans list if found
+            '''
             if token in N:
                 ans.append('N')
             if token in V:
@@ -108,23 +175,35 @@ class Sentence:
                 ans.append('J')
             if token in P:
                 ans.append('P')
+            if token in p:
+                ans.append('p')
+            '''
+            ans = scraper.pos(token)
+            if token in verbs:
+                ans.append('V')
             if ans == []:
                 print("No part of speech found for "+token)
+                ans.append('N')
             pos_list.append(ans)
         pos = list(product(*pos_list))
+        print(len(pos))
         return pos
     
             
     
+if __name__ == '__main__':
+    val = input()
+    sentence = Sentence(val.lower())
 
-val = input()
-sentence = Sentence(val.lower())
 
-
-for grammar in sentence.grammar_list:
-    print(grammar.pos)
-    print(grammar.temp)
-    print(grammar.spans)
-    print(grammar.span_pos)
-    print(grammar.final)
+    for grammar in sentence.grammar_list:
+        #print(grammar.pos)
+        print(grammar.final)
+        #print(grammar.spans)
+        #print(grammar.span_pos)
+        print(grammar.tree_token)
+        print(grammar.tree_pos)
+        #print(grammar.subject)
+        #print(grammar.verb)
+        #print(grammar.obj)
 
